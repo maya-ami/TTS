da7c06cc394233e7153242a659495d8f309afa6a
Maiia Bikmetova <maya.bikmetova@gmail.com>
2022-01-06 15:04:45 +0500

    fix path to train on kaggle

--- recipes/ljspeech/tacotron2-DDC/train_tacotron_ddc.py
+++ recipes/ljspeech/tacotron2-DDC/train_tacotron_ddc.py
@@ -14,7 +14,7 @@ output_path = os.path.dirname(os.path.abspath(__file__))
 
 # init configs
 dataset_config = BaseDatasetConfig(
-    name="ljspeech", meta_file_train="metadata.csv", path=os.path.join(output_path, "../LJSpeech-1.1/")
+    name="ljspeech", meta_file_train="metadata.csv", path=os.path.join(output_path, "/kaggle/input/ljspeech11/LJSpeech-1.1/")
 )
 
 audio_config = BaseAudioConfig(
